2ad455c33edf8536637098f0606c5059bc2b79b4
Gian Franco Stabile <gianfranco.stabile98@gmail.com>
2018-05-10 20:26:57 -0300

    Add files

---
+++ src/test/java/com/utn/tssi/tp5/ApiRestVuelos/ApiRestVuelosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.utn.tssi.tp5.ApiRestVuelos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiRestVuelosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
