2c30a61cb18cd87dfe67d93ba6b02965dbfd9dbd
Gian Franco Stabile <gianfranco.stabile98@gmail.com>
2018-05-30 10:41:52 -0300

    Add index view

--- ApiVuelos/src/main/java/com/ApiVuelos/ApiVuelos/repository/CabinRepository.java
+++ ApiVuelos/src/main/java/com/ApiVuelos/ApiVuelos/repository/CabinRepository.java
@@ -5,9 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-<<<<<<< HEAD
-public interface CabinRepository extends JpaRepository<Cabin , Long> {
-=======
 public interface CabinRepository extends JpaRepository<Cabin, Long> {
->>>>>>> alekano
 }
